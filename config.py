e86d88250f9b2785be801d27024db253582140a2
Sneha Kumari <snehabaranwal5613@gmail.com>
2026-03-12 00:38:40 +0530

    Comment out GOOGLE_API_KEY assignment

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import streamlit as st 
 
 ELEVENLABS_API_KEY = st.secrets["ELEVENLABS_API_KEY"]
-GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+# GOOGLE_API_KEY = st.secrets["GOOGLE_API_KEY"]
+MISTRAL_API_KEY = st.secrets["MISTRAL_API_KEY"]
 QDRANT_API_KEY = st.secrets["QDRANT_API_KEY"]
